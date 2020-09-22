0648e9ba182be05a3304a482bf93455aa8affefb
Md.Mozahidul Islam <71146918+mozahid1@users.noreply.github.com>
2020-09-22 20:35:23 +0600

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -3,7 +3,7 @@
 year = int(input("Enter a Year: "))
 
 if year % 400 == 0:
-    y = "{} is not a Leap Year."
+    y = "{} is a Leap Year."
     print(y.format(year))
 elif year % 100 == 0:
     y = "{} is not a Leap Year."
